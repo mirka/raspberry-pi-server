de57f3e62bf8384dde551169714a695da14af435
mirka <mirka@jaguchi.com>
2017-05-07 20:49:00 +0900

    Cache sunrise/sunset times for 24 hours

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const app = require('express')();
+const cacheProvider = require('./hue/models/cache');
 const hueScheduler = require('./hue/controllers/scheduler');
 
 const server = app.listen(8000, () => {
 	const port = server.address().port;
 	console.log('Listening at port %s', port);
 })
+
+
+// Start cache instance
+cacheProvider.init();
 
 // Set up cron-like Hue schedule on first launch
 hueScheduler();
--- hue/models/sun-times.js
+++ hue/models/sun-times.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const cacheProvider = require('./cache');
 const config = require('../config');
 
 const coords = config.sunTimesCoords;
@@ -8,34 +9,50 @@ const sunsetSunriseApiOptions = {
 	path: `/json?lat=${coords.lat}&lng=${coords.lng}&date=tomorrow&formatted=0`,
 }
 
+const sunTimesKey = 'sunTimes';
+const ttl = 86400; // 24 hrs
+
 
 function getSunsetSunriseTimes(options) {
+	const cache = cacheProvider.getInstance();
+
 	return new Promise((resolve, reject) => {
-		const req = https.request(options, (res) => {
-			const chunks = [];
-
-			res.on('data', (chunk) => {
-				chunks.push(chunk);
-			});
-
-			res.on('end', () => {
-				const body = Buffer.concat(chunks).toString();
-				const result = JSON.parse(body).results;
-				const times = {
-					sunrise: result.sunrise,
-					sunset: result.sunset,
-				}
-				resolve(times);
-			});
+		cache.get(sunTimesKey, (err, value) => {
+			if (!err && value) {
+				resolve(value);
+			} else {
+				callApi();
+			}
 		});
 
-		req.end();
+		function callApi() {
+			const req = https.request(options, (res) => {
+				const chunks = [];
+
+				res.on('data', (chunk) => {
+					chunks.push(chunk);
+				});
+
+				res.on('end', () => {
+					const body = Buffer.concat(chunks).toString();
+					const result = JSON.parse(body).results;
+					const times = {
+						sunrise: result.sunrise,
+						sunset: result.sunset,
+					}
+					cache.set(sunTimesKey, times, ttl);
+					resolve(times);
+				});
+			});
+
+			req.end();
+		}
 	});
 }
 
 module.exports = function() {
 	return getSunsetSunriseTimes(sunsetSunriseApiOptions)
-	.catch((err) => {
-		console.log(err);
-	});
+		.catch((err) => {
+			console.log(err);
+		});
 };
