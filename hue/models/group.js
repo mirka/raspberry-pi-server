5b24a1d94e411c863f72b4eacd4f7e17961d4017
mirka <mirka@jaguchi.com>
2017-05-08 06:32:15 +0900

    Implement smartToggle

--- hue/models/group.js
+++ hue/models/group.js
@@ -1,8 +1,8 @@
 const api = require('./hue-api.js');
 const lightState = require('node-hue-api').lightState;
+const sunTimes = require('./sun-times');
 const config = require('../config');
 
-const targetGroups = config.hue.adjustGroups;
 const Scene = {
 	DAYLIGHT: 'daylight',
 	EVENING: 'evening',
@@ -16,26 +16,35 @@ const states = {
 // Toggle group on/off
 function toggle(groupId) {
 	return toggleUsingFunction(groupId, () => {
-		api.setGroupLightState(groupId, states.on);
+		return api.setGroupLightState(groupId, states.on);
 	});
 }
 
 // Toggle group on/off, switching between scenes depending on time of day
 function smartToggle(groupId) {
 	return toggleUsingFunction(groupId, () => {
+		const targetGroups = config.hue.adjustGroups;
 		let target = targetGroups.find(findSceneForGroup);
 
-		if (true /* TODO: is daytime */) {
-			api.activateScene(target.scenes[Scene.DAYLIGHT]);
-		} else {
-			api.activateScene(target.scenes[Scene.EVENING]);
-		}
+		return isDaytime()
+			.then((isDaytimeNow) => {
+				if (isDaytimeNow) {
+					return api.activateScene(target.scenes[Scene.DAYLIGHT]);
+				} else {
+					return api.activateScene(target.scenes[Scene.EVENING]);
+				}
+			})
 	});
 
 	function findSceneForGroup(item) {
 		return item.id == groupId;
 	}
 }
+
+
+// ====================================
+// Utility
+// ====================================
 
 // onFunction decides what to do when group lights are currently off
 function toggleUsingFunction(groupId, onFunction) {
@@ -44,15 +53,36 @@ function toggleUsingFunction(groupId, onFunction) {
 			const group = groups[groupId];
 
 			if (group.state.any_on) {
-				api.setGroupLightState(groupId, states.off);
+				return api.setGroupLightState(groupId, states.off);
 			} else {
-				onFunction();
+				return onFunction();
 			}
 		})
 		.catch((err) => {
 			console.log(err);
 		});
 }
+
+function minutesSinceMidnight(time) {
+	return (time.getHours() * 60) + time.getMinutes();
+}
+
+// Returns whether current time is within sunrise and sunset
+function isDaytime() {
+	return sunTimes()
+		.then((times) => {
+			const now = minutesSinceMidnight(new Date());
+			const sunrise = minutesSinceMidnight(times.sunrise);
+			const sunset = minutesSinceMidnight(times.sunset);
+
+			if (now > sunrise && now < sunset) {
+				return true;
+			} else {
+				return false;
+			}
+		});
+}
+
 
 module.exports = {
 	toggle: toggle,
